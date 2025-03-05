506d193cd2703a3656954935bc6195f8abb44134
Alexey <alexey10zav@gmail.com>
2025-03-05 23:42:57 +0300

    replace path

--- src/main/java/org/example/Word2VecExample.java
+++ src/main/java/org/example/Word2VecExample.java
@@ -1,5 +1,4 @@
 package org.example;
-import org.slf4j.Logger;
 
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
@@ -9,11 +8,9 @@ import java.util.ArrayList;
 
 public class Word2VecExample {
     public static void main(String[] args) {
-        String home = System.getProperty("user.home");
-        String modelFolderPath = home + File.separator + "Desktop" + File.separator + "ai_models";
-        String pathToModel = modelFolderPath + File.separator + "GoogleNews-vectors-negative300.bin";
-
-        WordVectors model = WordVectorSerializer.readWord2VecModel(new File(pathToModel));
+        WordVectors model = WordVectorSerializer.readWord2VecModel(
+            new File("GoogleNews-vectors-negative300.bin")
+        );
 
         System.out.println(model.similarity("car", "bus"));
         System.out.println(model.similarity("yellow", "bus"));
